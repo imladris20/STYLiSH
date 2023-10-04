387d8f6c1499385c49101429a8443ce60c2ae47e
Po Lien Lin <polien.joe.lin@gmail.com>
2023-10-04 13:07:33 +0800

    Finish building all function and change deploy site

--- stylish/src/script/app.js
+++ stylish/src/script/app.js
@@ -4,7 +4,7 @@ async function fetchData(url) {
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Fetching calculating error:', error);
+    console.error("Fetching calculating error:", error);
     throw error;
   }
 }
@@ -12,47 +12,63 @@ async function fetchData(url) {
 const displayElement = (elementWithClass) => {
   const target = document.querySelector(`.${elementWithClass}`);
   target.style.display = "block";
-}
+};
 
 const hideElement = (elementWithClass) => {
   const target = document.querySelector(`.${elementWithClass}`);
-  target.style.display = 'none'
-}
+  target.style.display = "none";
+};
 
 const removeClassedElement = (elementClassName) => {
   const elementToRemove = document.querySelector(`.${elementClassName}`);
-  console.log("try removing");
   if (elementToRemove) {
-    console.log('It will remove');
     elementToRemove.remove();
   }
-}
+};
 
 const createClassedElement = (element, className) => {
   const newElement = document.createElement(element);
   newElement.className = className;
   return newElement;
-}
+};
 
 const renderProduct = (apiSourceData) => {
-  const main = document.querySelector('main');
-  const productContainer = createClassedElement("div", "main__product--container");
-  const productGrid = createClassedElement('div', "main__product product grid");
+  const main = document.querySelector("main");
+  const productContainer = createClassedElement(
+    "div",
+    "main__product--container"
+  );
+  const productGrid = createClassedElement("div", "main__product product grid");
 
   apiSourceData.forEach(({ main_image, colors, title, price }) => {
-    const productItem = createClassedElement('div', "product__item columnFlexBox");
-    const productItem_img = createClassedElement('img', "full-width");
-    const productItem_colorBox = createClassedElement('ul', "product__color--container rowFlexBox flex-space-between");
-    const productItem_title = createClassedElement('p', "product__description product__productItem_");
-    const productItem_price = createClassedElement('p', "product__description product__price");
+    const productItem = createClassedElement(
+      "div",
+      "product__item columnFlexBox"
+    );
+    const productItem_img = createClassedElement("img", "full-width");
+    const productItem_colorBox = createClassedElement(
+      "ul",
+      "product__color--container rowFlexBox flex-space-between"
+    );
+    const productItem_title = createClassedElement(
+      "p",
+      "product__description product__productItem_"
+    );
+    const productItem_price = createClassedElement(
+      "p",
+      "product__description product__price"
+    );
 
     //  Set image
     productItem_img.src = main_image;
-    productItem_img.alt = "product item"
+    productItem_img.alt = "product item";
 
     //  Produce and set colors
-    colors.forEach(color => {
-      const productItem_color = createClassedElement('li', "product__color--grid bd-lightgrey pointer");
+    colors.forEach((color) => {
+      const productItem_color = createClassedElement(
+        "li",
+        "product__color--grid bd-lightgrey pointer"
+      );
       productItem_color.style.backgroundColor = `#${color.code}`;
       productItem_colorBox.append(productItem_color);
     });
@@ -63,55 +79,95 @@ const renderProduct = (apiSourceData) => {
     //  Set price
     productItem_price.textContent = `TWD.${price}`;
 
-    productItem.append(productItem_img, productItem_colorBox, productItem_title, productItem_price);
-    -
-      productGrid.append(productItem);
-  })
+    productItem.append(
+      productItem_img,
+      productItem_colorBox,
+      productItem_title,
+      productItem_price
+    );
+    -productGrid.append(productItem);
+  });
 
   productContainer.append(productGrid);
 
-  main.append(productContainer)
+  main.append(productContainer);
 };
 
-const switchQueryString = (elementClicked) => {
-
-  const text = document.querySelector(`#${elementClicked}`)
-  const women = document.querySelector('.women');
-  const men = document.querySelector('.men');
-  const accessories = document.querySelector('.accessories');
-
-  const newQueryString = `category=${elementClicked}`;
-
-  // 獲取網址
-  const currentUrl = window.location.href;
-
-  // 檢查網址是否已經包含 query string
-  if (currentUrl.includes('?')) {
-    // 如果已經包含 query string，則替換它
-    const updatedUrl = currentUrl.replace(/category=[^&]+/, newQueryString);
-    window.history.pushState({}, '', updatedUrl);
-  } else {
-    // 如果尚未包含 query string，則新增它
-    const updatedUrl = currentUrl + '?' + newQueryString;
-    window.history.pushState({}, '', updatedUrl);
-  }
-}
-
-const fetchProduct = (category) => {
-  const apiHost = "https://api.appworks-school.tw/api";
-  const apiVersion = "/1.0";
-  const apiFunction = "/products";
-
-  fetchData(`${apiHost}${apiVersion}${apiFunction}/${category}`)
+const fetchProduct = ({ host, version, endpoint }, category) => {
+  fetchData(`${host}/${version}/${endpoint}/${category}`)
     .then(({ data, next_paging }) => {
+      removeClassedElement("main__product--container");
       renderProduct(data);
     })
-    .catch(error => {
-      console.error("Something went wrong while getting production information", error);
+    .catch((error) => {
+      console.error(
+        "Something went wrong while getting production information",
+        error
+      );
     })
     .finally(() => {
       hideElement("loadingGif");
-    })
-}
+    });
+};
 
-export { fetchData, fetchProduct, renderProduct, displayElement, hideElement, removeClassedElement, switchQueryString };
+const switchCategoryQuery = (categoryValue) => {
+  const newQueryString = `category=${categoryValue}`;
+  const currentUrl = window.location.href;
+
+  if (currentUrl.includes("?")) {
+    //  replace existed category value in query string
+    const updatedUrl = currentUrl.replace(/category=[^&]+/, newQueryString);
+    window.history.pushState({}, "", updatedUrl);
+  } else {
+    //  if not, add new
+    const updatedUrl = currentUrl + "?" + newQueryString;
+    window.history.pushState({}, "", updatedUrl);
+  }
+};
+
+const resetClassList = () => {
+  const elements = [women, men, accessories];
+  elements.forEach((element) => {
+    element.classList.replace("tx-white", "tx-grey82");
+    element.classList.replace("wider-tx-brown", "wider-tx-black3a");
+  });
+};
+
+const fetchProductByCategory = (sourceAPI) => {
+  displayElement("loadingGif");
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const categoryValue = urlParams.get("category");
+
+  switch (categoryValue) {
+    case "women":
+    case "men":
+    case "accessories":
+      resetClassList();
+      const selectedElement = document.querySelector(`#${categoryValue}`);
+      selectedElement.classList.replace("tx-grey82", "tx-white");
+      selectedElement.classList.replace("wider-tx-black3a", "wider-tx-brown");
+      fetchProduct(sourceAPI, categoryValue);
+      break;
+    case "all":
+    case null:
+      resetClassList();
+      fetchProduct(sourceAPI, "all");
+      break;
+    default:
+      resetClassList();
+      fetchProduct(sourceAPI, "all");
+      console.error("Invalid Query");
+  }
+};
+
+export {
+  fetchData,
+  fetchProduct,
+  fetchProductByCategory,
+  renderProduct,
+  displayElement,
+  hideElement,
+  removeClassedElement,
+  switchCategoryQuery,
+};
--- stylish/src/script/script.js
+++ stylish/src/script/script.js
@@ -1,53 +1,34 @@
-import { fetchData, fetchProduct, renderProduct, displayElement, hideElement, switchQueryString, removeClassedElement } from './app.js';
+import {
+  fetchProductByCategory,
+  displayElement,
+  switchCategoryQuery,
+} from "./app.js";
 
-const apiHost = "https://api.appworks-school.tw/api";
-const apiVersion = "/1.0";
-const apiFunction = "/products";
-const apiProductPaging = "";
+const stylishAPI = {
+  host: "https://api.appworks-school.tw/api",
+  version: "1.0",
+  endpoint: "products",
+  endpointPaging: "1",
+};
 
-/** Fetching Product according to category query */
-document.addEventListener('DOMContentLoaded', () => {
-  
-  displayElement("loadingGif");
+/** Initial fetching Product according to category query */
+document.addEventListener("DOMContentLoaded", () => {
+  fetchProductByCategory(stylishAPI);
+});
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const categoryValue = urlParams.get('category');
-
-  // 檢查是否成功取得值，如果存在該參數，categoryValue 會是該參數的值，否則會是 null
-  switch (categoryValue) {
-    case 'women':
-      console.log("fetch women");
-      fetchProduct('women');
-      break;
-    case 'men':
-      console.log("fetch men");
-      fetchProduct('men');
-      break;
-    case 'accessories':
-      console.log("fetch accessories");
-      fetchProduct('accessories');
-      break;
-    case 'all':
-    case null:
-      console.log("fetch all");
-      fetchProduct('all');
-      break;
-    default:
-      console.error("Invalid Query");
+/** Fetching product again when Categories be clicked */
+document.addEventListener("click", (event) => {
+  const categoryToChange = event.target.id;
+  const productType = ["women", "men", "accessories", "all"];
+  // console.log("you click: ", categoryToChange);
+  if (productType.includes(categoryToChange)) {
+    switchCategoryQuery(categoryToChange);
+    displayElement("loadingGif");
+    fetchProductByCategory(stylishAPI);
   }
-})
+});
 
-/** Fetching according to category query */
-document.addEventListener('click', event => {
-  // let elementClicked = "";
-  // console.log(event.target);
-  console.log(event.target.id);
-  const category = event.target.id;
-
-  switchQueryString(event.target.id);
-
-  displayElement("loadingGif");
-  removeClassedElement("main__product--container");
-
-  fetchProduct(category);
-})
+/** Make sure previous and next button still functioning */
+window.addEventListener("popstate", () => {
+  fetchProductByCategory(stylishAPI);
+});
