54ec94d20dbeab23f5a6f613e7a6c0381c25b400
Po Lien Lin <polien.joe.lin@gmail.com>
2023-10-11 16:51:20 +0800

    Initialize product.jsx

---
+++ stylish/src/components/Product.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { devices } from "../assets/device";
+
+const Product = () => {
+  return (
+    <>
+      <h1>我快睡著了</h1>
+    </>
+  )
+}
+
+export default Product;
