0b6bb61d179546f8067abb9e2563ed9f5985cc2d
Po Lien Lin <polien.joe.lin@gmail.com>
2023-10-13 17:04:11 +0800

    Update configurations of deploying

--- stylish/src/App.jsx
+++ stylish/src/App.jsx
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Product from "./components/Product"
+import NotFound from "./components/NotFound"
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Product />
+      <Routes>
+        <Route path="/product" element={<Product />} />
+        <Route path='*' element={<NotFound />}  />
+      </Routes>
       <Footer />
     </>
   );
--- stylish/vite.config.js
+++ stylish/vite.config.js
@@ -1,17 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
+import fs from "fs";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        stylish: './index.html',
+  plugins: [
+    react(),
+    {
+      writeBundle() {
+        fs.renameSync(
+          path.resolve(__dirname, "dist/index.html"),
+          path.resolve(__dirname, "dist/product.html")
+        );
+        fs.renameSync(
+          path.resolve(__dirname, "dist/homepage.html"),
+          path.resolve(__dirname, "dist/index.html")
+        );
       },
+    },
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "#root": resolve(__dirname),
     },
   },
   server: {
-    open: '/index.html'
-  }
-})
+    open: "/index.html",
+  },
+});
